38115734ce8ec3f2e27ecabc9883a79d0699b346
RomainKK <romain.kohnke.ma@gmail.com>
2024-12-05 19:06:27 +0100

    Factorial le repareyyy

--- math_challenges.py
+++ math_challenges.py
@@ -14,7 +14,16 @@ def math_challenge_factorial():
     n = random.randint(1,10)
 
     print(f"Math challenger: Calculate the factorial of {n} " )
-    X = int(input(f"Your answer is : "))
+
+    X = None
+    X_int = False 
+
+    while not X_int:
+        try:
+            X = int(input("Your answer is: "))
+            X_int = True
+        except ValueError:
+            print("Your answer should be an integer! Please try again.")
 
     if X == factorial(n):
         return(True)
